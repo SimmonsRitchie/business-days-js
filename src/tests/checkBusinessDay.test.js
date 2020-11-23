62af2c3f3bd32bb5e15ed4df6bfc4a95703ff1d8
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-11-23 13:41:48 +1300

    Build out tests

--- src/tests/checkBusinessDay.test.js
+++ src/tests/checkBusinessDay.test.js
@@ -6,7 +6,7 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 
 
-test("Determine Sun, Dec 25, 2016 is not a business day because it's a weekend", () => {
+test("Determine Dec 25, 2016 (Xmas day on Sunday) is not a business day because it's a weekend", () => {
   const dummyDate = dayjs.tz('2016-12-25', "America/New_York")
   const businessDay = isBusinessDay(dummyDate)
   expect(businessDay).toBe(false)
@@ -24,30 +24,43 @@ test("Determine Dec 27, 2016 is a business day because it's not a weekend or pub
   expect(businessDay).toBe(true)
 })
 
-test("Determine the string '2006-12-25' is not a business day", () => {
+test("Determine the string '2006-12-25' (Xmas day) is not a business day", () => {
   const dummyDate = '2016-12-25'
   const businessDay = isBusinessDay(dummyDate)
   expect(businessDay).toBe(false)
 })
 
-test("Determine the string '2006-12-26' is not a business day", () => {
+test("Determine the string '2006-12-26' (substitute for Xmas day) is not a business day", () => {
   const dummyDate = '2016-12-26'
   const businessDay = isBusinessDay(dummyDate)
   expect(businessDay).toBe(false)
 })
 
 
-test("Determine the string '2016-12-27' is a business day", () => {
+test("Determine the string '2016-12-27' (a Tuesday) is a business day", () => {
   const dummyDate = '2016-12-27'
   const businessDay = isBusinessDay(dummyDate)
   expect(businessDay).toBe(true)
 })
 
+test("Determine the string '2021-12-27' (a Wednesday) is a business day", () => {
+  const dummyDate = '2021-11-17'
+  const businessDay = isBusinessDay(dummyDate)
+  expect(businessDay).toBe(true)
+})
 
-test("Throw an error if input date is formatted as '27-12-2016", () => {
+
+test("Determine the string '2016-02-15' (Presidents Day) is not a business day", () => {
+  const dummyDate = dayjs.tz('2016-02-15', "America/New_York")
+  const businessDay = isBusinessDay(dummyDate)
+  expect(businessDay).toBe(false)
+})
+
+test("Throw an error if input date is formatted as '27-12-2016' is provided", () => {
   const dummyDate = '27-12-2016'
   expect(
     () => {
     isBusinessDay(dummyDate)
   }).toThrow()
 });
+
