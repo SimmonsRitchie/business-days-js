f13be731cceb9a2e1160c7ae62074d8ed9e1d855
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-11-25 14:58:21 +1300

    Use factory func, change API

--- src/index.js
+++ src/index.js
@@ -1,40 +1,78 @@
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc') // dependent on utc plugin
-const timezone = require('dayjs/plugin/timezone')
-var customParseFormat = require('dayjs/plugin/customParseFormat')
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.extend(customParseFormat)
-
-var Holidays = require('date-holidays')
-var hd = new Holidays()
-hd.init('US', 'pa')
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc"); // dependent on utc plugin
+const timezone = require("dayjs/plugin/timezone");
+var customParseFormat = require("dayjs/plugin/customParseFormat");
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
+var Holidays = require("date-holidays");
 
 
-export const isBusinessDay = (inputDate) => {
+const checkValidState = (stateAbbrv) => {
   /**
-   * Returns false if input date is on a weekend or a public holiday in Pennsylvania, USA.
+   * Logs warning to the console if a US State abbreviation is invalid
    * 
-   * @param {dayjs} date as a dayjs object
-   * @returns {bool} true if date is a weekend or holiday
+   * @param {str} stateAbbrv: State abbreviation. eg "pa"
+   * @returns {undefined}
    */
-  if (typeof inputDate === "string") {
-    try {
-      inputDate = dayjs.tz(inputDate, 'YYYY-MM-DD', "America/New_York")
-    } catch(e) {
-      throw "Could not parse date. Please provide either a dayjs object or a string formatted as 'YYYY-MM-DD'", e
-    }
+  const hd = new Holidays();
+  const stateList = Object.keys(hd.getStates('US'))
+  const stateInList = stateList.includes(stateAbbrv.toUpperCase())
+  if (!stateInList) {
+    throw `${stateAbbrv} is not a US state.`
   }
-
-  // Check if Sun (0) or Sat (6)
-  const dayOfWeek = inputDate.day()
-  if (dayOfWeek === 0 || dayOfWeek === 6) {
-    return false;
-  }
-  // Check if public holiday or substitute public holiday
-  const holidayObj = hd.isHoliday(inputDate.toDate())
-  if (holidayObj && holidayObj.type === 'public') { 
-    return false
-  }
-  return true;
 }
+
+const businessDays = (USState) => {
+  /**
+   * Factory function that creates a businessDays object.
+   *
+   * @param {str} USState: U.S. state to determine holidays
+   * @returns {str} true if date is a weekend or holiday
+   */
+  const cleanUSState = USState.toLowerCase()
+  checkValidState(cleanUSState)
+  const hd = new Holidays();
+  hd.init("US", cleanUSState);
+  return {
+    USState: cleanUSState,
+    getHolidays (year) {
+      /**
+       * Returns a list of all holidays for a given year.
+       */
+       return hd.getHolidays(year)
+    },
+    check(inputDate) {
+      /**
+       * Returns false if input date is on a weekend or a public holiday in Pennsylvania, USA.
+       *
+       * @param {dayjs} date as a dayjs object
+       * @returns {bool} true if date is a weekend or holiday
+       */
+
+      if (typeof inputDate === "string") {
+        try {
+          inputDate = dayjs.tz(inputDate, "YYYY-MM-DD", "America/New_York");
+        } catch (e) {
+          throw (
+            ("Could not parse date. Please provide either a dayjs object or a string formatted as 'YYYY-MM-DD'",
+            e)
+          );
+        }
+      }
+
+      // Check if Sun (0) or Sat (6)
+      const dayOfWeek = inputDate.day();
+      if (dayOfWeek === 0 || dayOfWeek === 6) {
+        return false;
+      }
+      // Check if public holiday or substitute public holiday
+      const holidayObj = hd.isHoliday(inputDate.toDate());
+      if (holidayObj && holidayObj.type === "public") {
+        return false;
+      }
+      return true;
+    },
+  };
+};
+export default businessDays;
