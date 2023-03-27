327263118a3fdec87b0c434b41762b9dbbca43eb
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2023-03-27 08:19:17 -0500

    Add typescript types

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+
+export interface CustomHoliday {
+    name: string;
+    rule: string;
+  }
+  
--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc"; // dependent on utc plugin
 import timezone from "dayjs/plugin/timezone";
 import customParseFormat from "dayjs/plugin/customParseFormat";
+import Holidays from "date-holidays";
+import { CustomHoliday } from "./types";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -10,10 +12,11 @@ dayjs.extend(customParseFormat);
 /**
  * Throws error to the console if a US State abbreviation is invalid, otherwise returns true
  *
- * @param {str} stateAbbrv: State abbreviation. eg "pa"
+ * @param {string} stateAbbrv: State abbreviation. eg "pa"
+ * @param {Holidays} hd: date-holidays instance
  * @returns {boolean} returns true if valid US state
  */
-export const validateState = (stateAbbrv, hd) => {
+export const validateState = (stateAbbrv: string, hd: Holidays): boolean => {
   let stateList = Object.keys(hd.getStates("US"));
   stateList = [...stateList, "US", "USA"];
   const stateInList = stateList.includes(stateAbbrv.toUpperCase());
@@ -26,16 +29,15 @@ export const validateState = (stateAbbrv, hd) => {
 /**
  * Checks that provided date is in valid format and returns date as a dayjs object.
  *
- * @param {string|Dayjs|Date} inputDate - date to validate
+ * @param {string| Date | dayjs.Dayjs} inputDate - date to validate
  * @returns {Dayjs} inputDate converted into Dayjs object.
  */
-export const validateDate = (inputDate) => {
+export const validateDate = (inputDate: string| Date | dayjs.Dayjs): dayjs.Dayjs => {
   try {
     inputDate = dayjs.tz(inputDate, "YYYY-MM-DD", "America/New_York");
   } catch (e) {
     throw (
-      ("Could not parse date. Please provide either a dayjs object, native Date object, or a string formatted as 'YYYY-MM-DD'",
-        e)
+      ("Could not parse date. Please provide either a dayjs object, native Date object, or a string formatted as 'YYYY-MM-DD'")
     );
   }
   return inputDate;
@@ -46,9 +48,10 @@ export const validateDate = (inputDate) => {
  *
  * @param {Holidays} hd - Holidays instance
  * @param {string} holName - name of holiday. eg. "Christmas Day"
+ * @param {string | number | Date} year - year to search, defaults to current year if not provided.
  * @returns {string}
  */
-export const getHolidayRule = (hd, holName, year) => {
+export const getHolidayRule = (hd: Holidays, holName: string, year?: string | number | Date): string => {
   const holList = hd.getHolidays(year);
   const hol = holList.find((item) => {
     return item.name.toLowerCase() === holName.toLowerCase();
@@ -63,12 +66,13 @@ export const getHolidayRule = (hd, holName, year) => {
  * Takes a Holidays instance and list of holidays and sets each holiday's type as 'optional' in the instance.
  * 
  * @param {Holidays} hd - Holidays instance
- * @param {Array.<string>} arrHols - array of holiday names. eg ["Christmas Day", "New Year's Day"]
- * @returns {undefined} 
+ * @param {string[]} arrHols - array of holiday names. eg ["Christmas Day", "New Year's Day"]
+ * @param {string | number | Date} year - year to search, defaults to current year if not provided.
+ * @returns {void} 
  */
-export const filterHolidays = (hd, arrHols) => {
+export const filterHolidays = (hd: Holidays, arrHols: string[], year?: string | number | Date): void => {
   arrHols.forEach((holName) => {
-    const rule = getHolidayRule(hd, holName, "2020");
+    const rule = getHolidayRule(hd, holName, year);
     if (rule) {
       hd.setHoliday(rule, { name: holName, type: "optional" });
     }
@@ -79,10 +83,10 @@ export const filterHolidays = (hd, arrHols) => {
  * Takes a Holidays instance and adds a list of custom public holidays.
  * 
  * @param {Holidays} hd - Holidays instance
- * @param {Array.<Object>} arrHols - list of objects representing custom holidays.
- * @returns {undefined} 
+ * @param {CustomHoliday[]} arrHols - list of objects representing custom holidays.
+ * @returns {void} 
  */
-export const addPublicHolidays = (hd, arrHols) => {
+export const addPublicHolidays = (hd: Holidays, arrHols: CustomHoliday[]): void => {
   arrHols.forEach(({ rule, name }) => {
     if (rule && name) {
       hd.setHoliday(rule, { name, type: "public" });
